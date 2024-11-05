ca5d82b3e2f1f8afd28e863b9a9a2b4a44dea119
Matejda1 <matejka.patri@gmail.com>
2024-11-05 21:41:32 +0100

    Add confirmed form text, Add font to All website

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,28 @@
 // Sending from form to Google sheets
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyeMMPBoUYkTrEPR3ZBPpkMBBQGShyQzv5nw4Q6JGaUEOvd1CbT3Bc2pHK7ZNfvBgRidA/exec'
 const form = document.forms['contact-form']
+const success = document.getElementById('success');
+const danger = document.getElementById('danger');
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => console.log('Success!', response))
+        .then(response => success.style.display = 'block')
         .catch(error => console.error('Error!', error.message))
+        .catch(error => danger.style.display = 'block')
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
 })
+
 
 // Delay onClick wait 2 second and delete text in form
 async function refreshPage() {
     console.log("waiting");
-    await sleep(1)
+    await sleep(2.5)
     document.getElementById("myForm").reset();
 }
 async function sleep(seconds) {
